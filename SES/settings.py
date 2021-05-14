6ffa3285730ba83f126041db0a00e33923bd344c
Lohith <190030919@kluniversity.in>
2021-05-14 20:51:42 +0530

    commit

--- SES/settings.py
+++ SES/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['127.0.0.1','stockexchangesystem.herokuapp.com','investngrow.he
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'SES.urls'
