3efb8cc215eb9cfa8b7eefea9d8530b6ca9c5762
Lohith <190030919@kluniversity.in>
2021-05-14 19:16:36 +0530

    commit

--- SES/settings.py
+++ SES/settings.py
@@ -150,7 +150,7 @@ MESSAGE_TAGS = {
 
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
